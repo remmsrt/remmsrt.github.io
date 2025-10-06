7c6014ac20179cfaf844ef9a42f4dbb567ed70ce
remmsrt <remisurat@outlook.com>
2025-10-06 15:36:50 +0000

    Deploying to gh-pages from @ remmsrt/remmsrt.github.io@55e408c7ebc12adf0cbd19f834e224a9015c02c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-genai",
           title: "GenAI",
-          description: "A selection of nice papers, blog posts and others about GenAI for continuous spaces.",
+          description: "A timeline of key papers and resources on generative AI for continuous spaces.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/genAI/";
